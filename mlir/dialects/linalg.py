c2eb3830bb639613814d40c8a267f908fad6fd27
Kaushik Kulkarni <kaushikcfd@gmail.com>
2020-12-22 10:23:02 -0600

    ssa_use -> ssa_id

--- mlir/dialects/linalg.py
+++ mlir/dialects/linalg.py
@@ -37,145 +37,145 @@ from mlir.dialect import Dialect, DialectOp, is_op
 
 class LinalgBatchMatmul(DialectOp):
     _syntax_ = [("linalg.batch_matmul"
-                 " ins( {a_id.ssa_use} , {b_id.ssa_use} : {a_type.type} , {b_type.type} )"
-                 " outs( {c_id.ssa_use} : {c_type.type} )"),
+                 " ins( {a_id.ssa_id} , {b_id.ssa_id} : {a_type.type} , {b_type.type} )"
+                 " outs( {c_id.ssa_id} : {c_type.type} )"),
                 ("linalg.batch_matmul"
-                 " ins( {a_id.ssa_use} , {b_id.ssa_use} : {a_type.type} , {b_type.type} )"
-                 " init( {init_id.ssa_use} : {init_type.type} ) -> {out_type.type}")]
+                 " ins( {a_id.ssa_id} , {b_id.ssa_id} : {a_type.type} , {b_type.type} )"
+                 " init( {init_id.ssa_id} : {init_type.type} ) -> {out_type.type}")]
 
 
 class LinalgConvW(DialectOp):
     _syntax_ = [("linalg.conv_1d"
-                 " ins( {in_id.ssa_use} , {filter_id.ssa_use} : {in_type.type} , {filter_type.type} )"
-                 " outs( {out_id.ssa_use} : {out_type.type} )")]
+                 " ins( {in_id.ssa_id} , {filter_id.ssa_id} : {in_type.type} , {filter_type.type} )"
+                 " outs( {out_id.ssa_id} : {out_type.type} )")]
 
 
 class LinalgConvHW(DialectOp):
     _syntax_ = [("linalg.conv_2d"
-                 " ins( {in_id.ssa_use} , {filter_id.ssa_use} : {in_type.type} , {filter_type.type} )"
-                 " outs( {out_id.ssa_use} : {out_type.type} )")]
+                 " ins( {in_id.ssa_id} , {filter_id.ssa_id} : {in_type.type} , {filter_type.type} )"
+                 " outs( {out_id.ssa_id} : {out_type.type} )")]
 
 
 class LinalgConvDHW(DialectOp):
     _syntax_ = [("linalg.conv_3d"
-                 " ins( {in_id.ssa_use} , {filter_id.ssa_use} : {in_type.type} , {filter_type.type} )"
-                 " outs( {out_id.ssa_use} : {out_type.type} )")]
+                 " ins( {in_id.ssa_id} , {filter_id.ssa_id} : {in_type.type} , {filter_type.type} )"
+                 " outs( {out_id.ssa_id} : {out_type.type} )")]
 
 
 class LinalgConv(DialectOp):
-    _syntax_ = [("linalg.conv( {in_id.ssa_use} , {filter_id.ssa_use} , {out_id.ssa_use} ) "
+    _syntax_ = [("linalg.conv( {in_id.ssa_id} , {filter_id.ssa_id} , {out_id.ssa_id} ) "
                 "{attr.attribute_value} : {in_type.type} , {filter_type.type} , {out_type.type}"),
-                ("linalg.conv( {in_id.ssa_use} , {filter_id.ssa_use} , {out_id.ssa_use} ) "
+                ("linalg.conv( {in_id.ssa_id} , {filter_id.ssa_id} , {out_id.ssa_id} ) "
                 " : {in_type.type} , {filter_type.type} , {out_type.type}")]
 
 
 class LinalgCopy(DialectOp):
-    _syntax_ = [("linalg.copy( {a_id.ssa_use} , {b_id.ssa_use} ) "
+    _syntax_ = [("linalg.copy( {a_id.ssa_id} , {b_id.ssa_id} ) "
                 "{attr.attribute_value} : {a_type.type} , {b_type.type}"),
-                ("linalg.copy( {a_id.ssa_use} , {b_id.ssa_use} ) "
+                ("linalg.copy( {a_id.ssa_id} , {b_id.ssa_id} ) "
                 " : {a_type.type} , {b_type.type}")]
 
 
 class LinalgDot(DialectOp):
     _syntax_ = [("linalg.dot"
-                 " ins( {in_a_id.ssa_use} , {in_b_id.ssa_use} : {in_a_type.type} , {in_b_type.type} )"
-                 " outs( {out_id.ssa_use} : {out_type.type} )")]
+                 " ins( {in_a_id.ssa_id} , {in_b_id.ssa_id} : {in_a_type.type} , {in_b_type.type} )"
+                 " outs( {out_id.ssa_id} : {out_type.type} )")]
 
 
 class LinalgFill(DialectOp):
-    _syntax_ = [("linalg.fill( {output_id.ssa_use} , {value_id.ssa_use} ) "
+    _syntax_ = [("linalg.fill( {output_id.ssa_id} , {value_id.ssa_id} ) "
                 "{attr.attribute_value} : {output_type.type} , {value_type.type}"),
-                ("linalg.fill( {output_id.ssa_use} , {value_id.ssa_use} ) "
+                ("linalg.fill( {output_id.ssa_id} , {value_id.ssa_id} ) "
                 " : {output_type.type} , {value_type.type}")]
 
 
 class LinalgGeneric(DialectOp):
     _syntax_ = [("linalg.generic {attr.attribute_value} "
-                 " ins( {inargs.ssa_use_list} : {in_types.type_list_no_parens} )"
-                 " outs( {outargs.ssa_use_list} : {out_types.type_list_no_parens} )"
+                 " ins( {inargs.ssa_id_list} : {in_types.type_list_no_parens} )"
+                 " outs( {outargs.ssa_id_list} : {out_types.type_list_no_parens} )"
                  " {body.region}"),
                 ("linalg.generic {attr.attribute_value} "
-                 " ins( {inargs.ssa_use_list} : {in_types.type_list_no_parens} )"
-                 " init( {init_args.ssa_use_list} : {init_types.type_list_no_parens} )"
+                 " ins( {inargs.ssa_id_list} : {in_types.type_list_no_parens} )"
+                 " init( {init_args.ssa_id_list} : {init_types.type_list_no_parens} )"
                  " {body.region} -> {out_type.type}")]
 
 
 class LinalgIndexedGeneric(DialectOp):
     _syntax_ = [("linalg.indexed_generic {attr.attribute_value} "
-                 " ins( {inargs.ssa_use_list} : {in_types.type_list_no_parens} )"
-                 " outs( {outargs.ssa_use_list} : {out_types.type_list_no_parens} )"
+                 " ins( {inargs.ssa_id_list} : {in_types.type_list_no_parens} )"
+                 " outs( {outargs.ssa_id_list} : {out_types.type_list_no_parens} )"
                  " {body.region}"),
                 ("linalg.indexed_generic {attr.attribute_value} "
-                 " ins( {inargs.ssa_use_list} : {in_types.type_list_no_parens} )"
-                 " init( {init_args.ssa_use_list} : {init_types.type_list_no_parens} )"
+                 " ins( {inargs.ssa_id_list} : {in_types.type_list_no_parens} )"
+                 " init( {init_args.ssa_id_list} : {init_types.type_list_no_parens} )"
                  " {body.region} -> {out_type.type}")]
 
 
 class LinalgRange(DialectOp):
-    _syntax_ = [("linalg.range {min_id.ssa_use} : {max_id.ssa_use} : {step_id.ssa_use}"
+    _syntax_ = [("linalg.range {min_id.ssa_id} : {max_id.ssa_id} : {step_id.ssa_id}"
                  " {attr.attribute_value} : {out_type.type}"),
-                ("linalg.range {min_id.ssa_use} : {max_id.ssa_use} : {step_id.ssa_use}"
+                ("linalg.range {min_id.ssa_id} : {max_id.ssa_id} : {step_id.ssa_id}"
                  " : {out_type.type}")]
 
 
 class LinalgReshape(DialectOp):
-    _syntax_ = [("linalg.reshape {src_id.ssa_use}"
+    _syntax_ = [("linalg.reshape {src_id.ssa_id}"
                  " [ {reassociation.affine_map_list} ] "
                  " {attr.attribute_value} "
                  " : {src_type.memref_type} into {result_type.memref_type}"),
-                ("linalg.reshape {src_id.ssa_use}"
+                ("linalg.reshape {src_id.ssa_id}"
                  " [ ] "
                  " {attr.attribute_value} "
                  " : {src_type.memref_type} into {result_type.memref_type}"),
-                ("linalg.reshape {src_id.ssa_use}"
+                ("linalg.reshape {src_id.ssa_id}"
                  " [ {reassociation.affine_map_list} ] "
                  " : {src_type.memref_type} into {result_type.memref_type}"),
-                ("linalg.reshape {src_id.ssa_use}"
+                ("linalg.reshape {src_id.ssa_id}"
                  " [ ] "
                  " : {src_type.memref_type} into {result_type.memref_type}")]
 
 
 class LinalgSlice(DialectOp):
-    _syntax_ = ("linalg.slice {view_id.ssa_use} [ {indexing_ids.ssa_use_list} ]"
+    _syntax_ = ("linalg.slice {view_id.ssa_id} [ {indexing_ids.ssa_id_list} ]"
                 " : {view_type.type} , {indexing_types.type_list_no_parens} "
                 " , {result_type.type}")
 
 
 class TensorReshape(DialectOp):
-    _syntax_ = [("linalg.tensor_reshape {src_id.ssa_use}"
+    _syntax_ = [("linalg.tensor_reshape {src_id.ssa_id}"
                  " [ {reassociation.affine_map_list} ] "
                  " {attr.attribute_value} "
                  " : {src_type.tensor_type} into {result_type.tensor_type}"),
-                ("linalg.tensor_reshape {src_id.ssa_use}"
+                ("linalg.tensor_reshape {src_id.ssa_id}"
                  " [ ] "
                  " {attr.attribute_value} "
                  " : {src_type.tensor_type} into {result_type.tensor_type}"),
-                ("linalg.tensor_reshape {src_id.ssa_use}"
+                ("linalg.tensor_reshape {src_id.ssa_id}"
                  " [ {reassociation.affine_map_list} ] "
                  " : {src_type.tensor_type} into {result_type.tensor_type}"),
-                ("linalg.tensor_reshape {src_id.ssa_use}"
+                ("linalg.tensor_reshape {src_id.ssa_id}"
                  " [ ] "
                  " : {src_type.tensor_type} into {result_type.tensor_type}")]
 
 
 class LinalgYield(DialectOp):
-    _syntax_ = ("linalg.yield {operand_ids.ssa_use_list}"
+    _syntax_ = ("linalg.yield {operand_ids.ssa_id_list}"
                 " : {operand_types.type_list_no_parens}")
 
 
 class LinalgMatmul(DialectOp):
     _syntax_ = [("linalg.matmul"
-                 " ins( {a_id.ssa_use} , {b_id.ssa_use} : {a_type.type} , {b_type.type} )"
-                 " outs( {c_id.ssa_use} : {c_type.type} )"),
+                 " ins( {a_id.ssa_id} , {b_id.ssa_id} : {a_type.type} , {b_type.type} )"
+                 " outs( {c_id.ssa_id} : {c_type.type} )"),
                 ("linalg.matmul"
-                 " ins( {a_id.ssa_use} , {b_id.ssa_use} : {a_type.type} , {b_type.type} )"
-                 " init( {init_id.ssa_use} : {init_type.type} )  -> {out_type.type}")]
+                 " ins( {a_id.ssa_id} , {b_id.ssa_id} : {a_type.type} , {b_type.type} )"
+                 " init( {init_id.ssa_id} : {init_type.type} )  -> {out_type.type}")]
 
 
 class LinalgMatvec(DialectOp):
     _syntax_ = [("linalg.matvec"
-                 " ins( {a_id.ssa_use} , {b_id.ssa_use} : {a_type.type} , {b_type.type} )"
-                 " outs( {c_id.ssa_use} : {c_type.type} )")]
+                 " ins( {a_id.ssa_id} , {b_id.ssa_id} : {a_type.type} , {b_type.type} )"
+                 " outs( {c_id.ssa_id} : {c_type.type} )")]
 
 
 # Inspect current module to get all classes defined above
